a162c1bed6175912a3c487299ed057603b4dd5e9
Alison Tristão <alisontristao@hotmail.com>
2024-09-05 14:06:13 -0300

    teste

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,8 @@
 #include <Telemetry.h>
 #include <Error.h>
 
+// aloo
+
 // state machine
 StateMachine state1(SETUP, setup_function, next_state_setup);
 StateMachine state2(WAIT, wait_function, next_state_wait);	
